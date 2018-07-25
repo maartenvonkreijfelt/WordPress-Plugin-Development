2cd1182da57cff74befdb21a51a72379fea31309
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-07-25 13:37:29 +0200

    update

---
+++ prebrandit/prebrandit.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name:  Pre Brandit
+Description:  Example plugin for the video tutorial series, "WordPress: Plugin Development", available at Lynda.com.
+Plugin URI:   https://www.preludio.nl/plugins/prebrandit
+Author:       Maarten von Kreijfelt
+Version:      1.0
+Text Domain:  prebrandit
+Domain Path:  /languages
+License:      GPL v2 or later
+License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
+*/
