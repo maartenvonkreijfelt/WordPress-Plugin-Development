4e95539683bfd6ae0ff8f0bdd93b7c65a311fce1
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-08-31 14:52:44 +0200

    update

---
+++ myplugin/myplugin.php
@@ -0,0 +1,11 @@
+/*
+Plugin Name:  My Plugin
+Description:  Demo plugin belonging to tutorial Plugin development part 4
+Plugin URI:
+Author:       Maarten von Kreijfelt
+Version:      1.0
+Text Domain:  myplugin
+Domain Path:  /languages
+License:      GPL v2 or later
+License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
+*/
